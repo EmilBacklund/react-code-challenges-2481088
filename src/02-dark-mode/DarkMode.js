e5930ba81b6a6db23fcce8c4b1995a36c138ed95
emil backlund <emil_backi_@hotmail.com>
2023-04-04 23:26:15 +0200

    complete 2nd challenge
    dark mode challenge

--- src/02-dark-mode/DarkMode.js
+++ src/02-dark-mode/DarkMode.js
@@ -1,8 +1,30 @@
-export default function DarkMode () {
+import { useState } from 'react';
+
+export default function DarkMode() {
+  const [mode, setMode] = useState('lightMode');
+
+  switch (mode) {
+    case 'lightMode':
+      document.querySelector('.page').classList.remove('dark-mode');
+      break;
+    case 'darkMode':
+      document.querySelector('.page').classList.add('dark-mode');
+      break;
+    default:
+      console.log('Something went wrong');
+  }
+
   return (
-    <div className='page'>
-      <button className='dark-mode-button'>Dark Mode</button>
-      <button className='light-mode-button'>Light Mode</button>
+    <div className="page">
+      <button onClick={() => setMode('darkMode')} className="dark-mode-button">
+        Dark Mode
+      </button>
+      <button
+        onClick={() => setMode('lightMode')}
+        className="light-mode-button"
+      >
+        Light Mode
+      </button>
     </div>
-  )
+  );
 }
